35b571658b27542a99471c212693d97bbf8add2f
SomeoneHX <baohouxun@163.com>
2026-08-02 22:47:16 +0800

    ci: deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/RulePixel/',
   plugins: [vue()],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
